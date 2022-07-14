d72aa8082a44db8f6c4d9f7610496f6ca618a4fb
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2022-07-14 16:39:02 +0300

    7-closure

--- js/7-callback.js
+++ js/7-callback.js
@@ -43,3 +43,96 @@
 // console.log(r3);
 
 
+
+// // Task 2
+// //  Поваренок
+// const makeDish = function (sheffName, dish) {
+//     console.log(`${sheffName} готовит ${dish}`);
+// };
+
+// makeDish('Mango', 'пирожок');
+// makeDish('Mango', 'омлет');
+// makeDish('Mango', 'чай');
+
+// makeDish('Poly', 'котлеты');
+// makeDish('Poly', 'супик');
+// makeDish('Poly', 'кофе');
+
+// const makeSheff = function (name) {
+//     const makeDish = function (dish) {
+//         console.log(`${name} готовит ${dish}`);
+//     }
+
+//     return makeDish;
+//  };
+
+// const mango = makeSheff('Mango');
+
+// mango('котлеты');
+// mango('пирожок');
+
+// const poly = makeSheff('Poly');
+
+// poly('омлет');
+// poly('чай');
+
+
+
+// // Task 3
+// //  Округлятор
+// // const floatingPoint = 3.456789;
+// // const someInt = Math.round(floatingPoint);
+// // const withDecimals = Number(floatingPoint.toFixed(2));
+
+// // console.log(Number(floatingPoint.toFixed(3)));
+// // console.log(Number(floatingPoint.toFixed(4)));
+
+// // rounder2(floatingPoint);
+// // rounder3(floatingPoint);
+
+// const rounder = function (places) {
+//     return function (number) {
+//         return Number(number.toFixed(places));
+//     }
+// }
+// const rounder2 = rounder(2);
+// const rounder3 = rounder(3);
+
+// console.log(rounder2(3.56788));
+// console.log(rounder3(3.56788));
+// console.log(rounder2(4.90978));
+// console.log(rounder3(4.90978));
+
+
+// Task 4
+//  Зарплата
+const salaryManagerFactory = function (employeeName, baseSalary = 0) {
+    let salary = baseSalary;
+
+    return {
+        raise(amount) {
+            if (amount > 1000) {
+                return `${employeeName} - Ты офигел?`
+            }
+            salary += amount;
+        },
+        lower(amount) {
+            salary -= amount;
+        },
+        current() {
+            return `Текущая зарплата ${employeeName} - ${salary}`;
+        },
+    };
+};
+
+const salaryManager = salaryManagerFactory('Игорь:', 800);
+console.log(salaryManagerFactory())
+// console.log(salaryManager.current());
+
+// console.log(salaryManager.raise(1100));
+// console.log(salaryManager.current());
+
+
+
+
+
