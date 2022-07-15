8120cabae7f6ed96a97fe1388251b67580f7ba5a
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2022-07-15 21:46:50 +0300

    8-filter-find

--- js/8-iterating.js
+++ js/8-iterating.js
@@ -72,12 +72,38 @@
 // console.table(updatedPlayers);
 
 
-// Task 4
-// метод  " filter() "
+// // Task 4
+// // метод  " filter() "
+// const numbers = [5, 10, 15, 20, 25];
+
+// const filteredNumbers = numbers.filter(number =>  number < 10 || number > 20);
+// console.log(filteredNumbers);
+
+    
+
+// const players = [
+//     { id: 'player - 1', name: 'Mango', timePlayed: 310, points: 54, online: false},
+//     { id: 'player - 2', name: 'Poly', timePlayed: 470, points: 92, online: true},
+//     { id: 'player - 3', name: 'Kiwi', timePlayed: 230, points: 48, online: true},
+//     { id: 'player - 4', name: 'Ajax', timePlayed: 150, points: 71, online: false},
+//     { id: 'player - 5', name: 'Chelsy', timePlayed: 80, points: 48, online: true},
+// ];
+
+// // Получаем массив всех онлайн игроков
+// const onlinePlayers = players.filter(({online}) => online);
+// console.table(onlinePlayers);
+
+// const offlinePlayers = players.filter(player => !player.online);
+// // console.table(offlinePlayers);
+
+
+
+// Task 5
+// метод  " find() "
 const numbers = [5, 10, 15, 20, 25];
 
-const filteredNumbers = numbers.filter(number =>  number < 10 || number > 20); 
-console.log(filteredNumbers);
+const number = numbers.find(number =>  number > 10); 
+console.log(number);
 
     
 
@@ -88,3 +114,17 @@ const players = [
     { id: 'player - 4', name: 'Ajax', timePlayed: 150, points: 71, online: false},
     { id: 'player - 5', name: 'Chelsy', timePlayed: 80, points: 48, online: true},
 ];
+
+// Ищем игрока по id
+const playerIdFind = 'player - 3';
+
+const playerWithId = players.find(({id}) => id === playerIdFind);
+console.log(playerWithId);
+
+
+ 
+const findPlayerById = (allPlayers, playerId) =>
+ allPlayers.find(({ id }) => id === playerId);
+
+console.log(findPlayerById(players, 'player - 1'));
+console.log(findPlayerById(players, 'player - 4'));
