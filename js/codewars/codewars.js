23454c8c889a5dba5066de67bbe26f051a2fb8e4
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-01-10 22:48:00 +0200

    10.01

--- js/codewars/codewars.js
+++ js/codewars/codewars.js
@@ -1,12 +1,32 @@
-function findAverage(array) {
+// function findAverage(array) {
 
-    if (array.length !== 0) {
-     const sum = array.reduce((prevNumber, number) => {return prevNumber + number}, 0);
-     const averageNumbers = sum/array.length;
+//     if (array.length !== 0) {
+//      const sum = array.reduce((prevNumber, number) => {return prevNumber + number}, 0);
+//      const averageNumbers = sum/array.length;
     
-     console.log(averageNumbers);
+//      console.log(averageNumbers);
    
-    } else {console.log(0)};
-   }
+//     } else {console.log(0)};
+//    }
 
-   findAverage([1, 2, 3]);
+//    findAverage([1, 2, 3]);
+
+// function grow(x){
+//    const multiply = x.reduce((prevNumber, number) => prevNumber * number)
+//    console.log(multiply) 
+// }
+
+// grow([1, 2, 3, 4])
+
+function summa(num) {
+   // Code here
+  let sum = 0
+   for (let i = 1; i <= num; i +=1){
+    sum += i
+   
+    
+   
+   }
+   console.log(sum)
+ }
+summa(3)
