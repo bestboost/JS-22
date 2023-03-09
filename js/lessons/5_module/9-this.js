c3da81cfb5d39840d9582c218060e2e222c55c57
Darya Hudzovska <102170750+bestboost@users.noreply.github.com>
2023-03-09 21:58:49 +0200

    +

--- js/lessons/5_module/9-this.js
+++ js/lessons/5_module/9-this.js
@@ -102,7 +102,7 @@
 //      },
 //      discrement(value) {
 //           console.log('disrement -> this', this);
-//           this.value += value;
+//           this.value -= value;
 //      },
 // };
 
@@ -110,50 +110,134 @@
 //      operation(value);
 // }
 
-// updateCounter(10, counter.increment);
-// updateCounter(5, counter.discrement);
+// updateCounter(10, counter.increment.bind(counter));
+// updateCounter(5, counter.discrement.bind(counter));
+// console.log(counter)
 
-// 
 
-const atTheOldToad = {
-  potions: [
-    { name: "Speed potion", price: 460 },
-    { name: "Dragon breath", price: 780 },
-    { name: "Stone skin", price: 520 },
-  ],
-  // Change code below this line
-  getPotions() {
-    return this.potions;
-  },
-  addPotion(newPotion) {
-    for (const potion of this.potions) {
-        if (potion.name.includes(newPotion.name)) {
-      return `Error! Potion ${newPotion.name} is already in your inventory!`;
-             }
-       }
-    this.potions.push(newPotion);
-  },
 
-  removePotion(potionName) {
- for (const potion of this.potions){
 
-    if (potion.name === potionName) {
-    this.potions.splice(this.potions.indexOf(potion), 1)
-      }
-}
-      },
-      
-  updatePotionName(oldName, newName) {
+// const showThis = function (...args) {
+//   console.log(args)
+//   console.log('changeColor -> this', this);
+// };
 
-   for (const potion of this.potions) {
+// showThis();
 
-   if (potion.name === oldName) {
-    // console.log(Object.values(potion)[0])
-    // console.log(Object.values(potion)[0].indexOf(potion.name))
-    console.log(potion.name = newName);
-   }
+// const objA = {
+//   a: 5,
+//   b:25,
+// }
+// showThis.call(objA, 2,3,4,10);
+// showThis.apply(objA, [1,2,3]);
+
+// const objB = {
+//   x: 15,
+//   y: 50,
+// }
+
+// showThis.call(objB, 11,12,13,14);
+// showThis.apply(objB, [11,12,13])
+// showThis();
+
+
+// const changeColor = function (color) {
+//             console.log('changeColor -> this', this);
+//             this.color = color;
+//        };
   
-   }
+//   const hat = {
+//        color: 'black',
+//   };
+// // changeColor.call(hat, 'orange');
+// // console.log(hat)
+
+//   const sweater = {
+//     color: 'green',
+//   }
+//   // changeColor.call(sweater, 'blue');
+//   // console.log(sweater);
+
+// const changeHatColor = changeColor.bind(hat);
+// const changeSweatwerColor = changeColor.bind(sweater);
+
+// changeHatColor('yelowe');
+// console.log(hat);
+
+// changeSweatwerColor('rad');
+// console.log(sweater);
+
+// const counter = {
+//   value: 0,
+//   increment() {
+//        console.log('increment -> this', this);
+//        this.value += 1;
+//   },
+//   discrement() {
+//        console.log('disrement -> this', this);
+//        this.value -= 1;
+//   },
+// };
+
+// const decrementBtn = document.querySelector('.js-decrement');
+// const incrementBtn = document.querySelector('.js-increment');
+// const valueEl = document.querySelector('.js-value');
+
+// console.log(decrementBtn);
+// console.log(incrementBtn);
+// console.log(valueEl);
+
+// decrementBtn.addEventListener('click', function() {
+//   counter.discrement();
+//   console.log(counter);
+//   valueEl.textContent = counter.value;
+// });
+
+// incrementBtn.addEventListener('click', function() {
+//   counter.increment();
+//   console.log(counter);
+//   valueEl.textContent = counter.value;
+// });
+
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Dragon breath", price: 780 },
+//     { name: "Stone skin", price: 520 },
+//   ],
+//   // Change code below this line
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(newPotion) {
+//     for (const potion of this.potions) {
+//         if (potion.name.includes(newPotion.name)) {
+//       return `Error! Potion ${newPotion.name} is already in your inventory!`;
+//              }
+//        }
+//     this.potions.push(newPotion);
+//   },
+
+//   removePotion(potionName) {
+//  for (const potion of this.potions){
+
+//     if (potion.name === potionName) {
+//     this.potions.splice(this.potions.indexOf(potion), 1)
+//       }
+// }
+//       },
+      
+//   updatePotionName(oldName, newName) {
+
+//    for (const potion of this.potions) {
+
+//    if (potion.name === oldName) {
+//     // console.log(Object.values(potion)[0])
+//     // console.log(Object.values(potion)[0].indexOf(potion.name))
+//     console.log(potion.name = newName);
+//    }
+  
+//    }
    
 //     const potionIndex = this.potions.indexOf(oldName);
 // console.log(potionIndex)
@@ -162,7 +246,36 @@ const atTheOldToad = {
     // }
 
     // console.log(this.potions.splice(potionIndex, 1, newName));
+  // }
+  // Change code above this line
+// };
+// atTheOldToad.updatePotionName("Dragon breath", "Polymorth");
+
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  
+  getItems() {
+  return this.items;
+  }
+  
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+  
+  removeItem(itemToRemove) {
+   console.log(this.items.splice(this.items.findIndex(item => item === itemToRemove)
+   ), 1);
+  }
+  // removeItem(itemToRemove) {
+  //   this.items.splice(this.items.findIndex(item => item === itemToRemove), 1);
+  //  }
   }
   // Change code above this line
-};
-atTheOldToad.updatePotionName("Dragon breath", "Polymorth");
+  const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+  console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+  storage.addItem("Droid");
+  console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+  storage.removeItem("Prolonger");
+  console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
